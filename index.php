15001f31ec7e445ce8d489af656ae0e41c7e0142
Saad Alshrideh <s14192009@gmail.com>
2021-05-26 11:46:31 +0300

    first try on this thing

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+    <head>
+        <title>Whatever i'm just trying !!</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+
+
+   <body class="bg-yellow-450">
+        <h1>Whatever</h1>
+   </body>
+
+</html>
